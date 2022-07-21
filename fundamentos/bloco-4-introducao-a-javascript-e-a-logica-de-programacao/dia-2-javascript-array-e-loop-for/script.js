2359652e1e6e4f4a444926437cd5a46df9885348
Leila Moraes <leila.violino@gmail.com>
2022-07-21 20:17:05 -0300

    resolução do exercício 2

--- fundamentos/bloco-4-introducao-a-javascript-e-a-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-a-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -5,3 +5,8 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // Exercício 1
 
 console.log(numbers);
+
+// Exercício 2
+
+console.log(numbers.length);
+console.log(numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + numbers[7] + numbers[8] + numbers[9]);
