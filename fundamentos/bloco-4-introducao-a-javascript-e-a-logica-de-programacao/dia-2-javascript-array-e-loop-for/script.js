eb0cb6917febc150162353057ed6a2032e5f46dd
Leila Moraes <leila.violino@gmail.com>
2022-07-21 20:24:17 -0300

    resolução do exercício 3

--- fundamentos/bloco-4-introducao-a-javascript-e-a-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-a-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -10,3 +10,10 @@ console.log(numbers);
 
 console.log(numbers.length);
 console.log(numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + numbers[7] + numbers[8] + numbers[9]);
+
+// Exercício 3
+
+let lenght = numbers.length
+let sum = numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + numbers[7] + numbers[8] + numbers[9]
+
+console.log(sum / lenght);
