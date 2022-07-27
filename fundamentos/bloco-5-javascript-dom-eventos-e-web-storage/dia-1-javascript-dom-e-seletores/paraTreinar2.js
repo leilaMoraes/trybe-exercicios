c79500752eb73b97278573d410274a3a04a3a6e4
Leila Moraes <leila.violino@gmail.com>
2022-07-27 20:02:50 -0300

    finalizão da padronização do site de acordo com a imagem, colocando o background nos títulos h3

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/paraTreinar2.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/paraTreinar2.js
@@ -5,3 +5,11 @@ document.getElementById('footer-container').style.backgroundColor = 'rgb(0, 50, 
 document.querySelector('.emergency-tasks').style.backgroundColor = 'rgb(255, 160, 133)'
 
 document.querySelector('.no-emergency-tasks').style.backgroundColor = 'rgb(249, 220, 94)';
+
+document.querySelectorAll('h3')[0].style.backgroundColor = 'rgb(166, 0, 243)';
+
+document.querySelectorAll('h3')[1].style.backgroundColor = 'rgb(166, 0, 243)';
+
+document.querySelectorAll('h3')[2].style.backgroundColor = 'rgb(49, 48, 42)';
+
+document.querySelectorAll('h3')[3].style.backgroundColor = 'rgb(49, 48, 42)';
