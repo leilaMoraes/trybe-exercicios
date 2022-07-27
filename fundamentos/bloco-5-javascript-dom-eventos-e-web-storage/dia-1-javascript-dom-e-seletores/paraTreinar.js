81acdad4c333ab3fc182601b42557533983220b6
Leila Moraes <leila.violino@gmail.com>
2022-07-27 16:23:29 -0300

    exercício 2-parte 2 concluído

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/paraTreinar.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/paraTreinar.js
@@ -3,3 +3,5 @@ document.getElementById("page-title").innerText = 'High School Musical 3';
 document.getElementById("second-paragraph").innerText = "Em meio a preparativos para um campeonato de basquete, baile e formatura, os namorados Troy Bolton e Gabriella Montez curtem cada momento, sabendo que a faculdade coloca o futuro de seu relacionamento em jogo. Um elaborado musical de primavera é apenas mais uma oportunidade para os dois Wildcats e seus colegas expressarem os medos e as esperanças para o futuro.";
 
 document.getElementById("subtitle").innerText = "Ano da Formatura"
+
+document.getElementsByClassName("paragraphs");
