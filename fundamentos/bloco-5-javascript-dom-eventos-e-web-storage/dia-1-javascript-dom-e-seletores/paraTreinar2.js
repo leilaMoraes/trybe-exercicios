40dbc99258053af9be6e2c851e132be5b699f2c4
Leila Moraes <leila.violino@gmail.com>
2022-07-27 19:24:30 -0300

    correção do tom das cores

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/paraTreinar2.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/paraTreinar2.js
@@ -1,3 +1,7 @@
-document.getElementById('header-container').style.backgroundColor = "rgb(116, 197, 136)";
+document.getElementById('header-container').style.backgroundColor = 'rgb(0, 178, 105)';
 
-document.getElementById('footer-container').style.backgroundColor = "rgb(3, 57, 51)";
+document.getElementById('footer-container').style.backgroundColor = 'rgb(0, 50, 48)';
+
+document.querySelector('.emergency-tasks').style.backgroundColor = 'rgb(255, 160, 133)'
+
+document.querySelector('.no-emergency-tasks').style.backgroundColor = 'rgb(249, 220, 94)';
