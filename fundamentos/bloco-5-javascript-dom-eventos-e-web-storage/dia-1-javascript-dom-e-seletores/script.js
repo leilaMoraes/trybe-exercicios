7679dd48b10124529955a5472c570ba38348765c
Leila Moraes <leila.violino@gmail.com>
2022-07-27 23:13:18 -0300

    finalização do exercício 3

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -1,3 +1,6 @@
 document.getElementsByTagName('p')[1].innerText = 'Eu imagino que eu esteja trabalhando em uma ótima empresa como DEV e que por trabalhar remoto eu já tenha conhecido vários lugares diferentes e esteja codando de frente pro mar, onde darei um mergulho no final do dia';
 
 document.getElementsByClassName('main-content')[0].style.backgroundColor = 'rgb(76,164,109)';
+
+document.getElementsByClassName('center-content')[0].style.backgroundColor = 'white';
+
