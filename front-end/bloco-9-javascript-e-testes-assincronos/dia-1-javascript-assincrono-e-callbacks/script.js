94bcb1b5b170a564ea2fc4d7500413ed8f1a3a55
Leila Moraes <leila.violino@gmail.com>
2022-09-06 20:12:25 -0300

    resolução da questão 2

--- front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/script.js
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/script.js
@@ -31,3 +31,38 @@ const jupiter = {
 console.log(planetDistanceFromSun(mars)); // A primeiro
 console.log(planetDistanceFromSun(venus)); // B segundo
 console.log(planetDistanceFromSun(jupiter)); // C terceiro
+
+// Exercício 2
+
+const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
+  `${name} está a ${value} ${measurementUnit} de distância do Sol`;
+
+const MEASUREMENT_UNIT = 'quilômetros';
+
+const mars = {
+  name: 'Marte',
+  distanceFromSun: {
+    value: 227900000,
+    measurementUnit: MEASUREMENT_UNIT,
+  },
+};
+
+const venus = {
+  name: 'Venus',
+  distanceFromSun: {
+    value: 108200000,
+    measurementUnit: MEASUREMENT_UNIT,
+  },
+};
+
+const jupiter = {
+  name: 'Jupiter',
+  distanceFromSun: {
+    value: 778500000,
+    measurementUnit: MEASUREMENT_UNIT,
+  },
+};
+
+console.log(planetDistanceFromSun(mars)); // A primeiro
+setTimeout(() => console.log(planetDistanceFromSun(venus)), 3000); // B terceiro
+setTimeout(() => console.log(planetDistanceFromSun(jupiter)), 2000); // C segundo
