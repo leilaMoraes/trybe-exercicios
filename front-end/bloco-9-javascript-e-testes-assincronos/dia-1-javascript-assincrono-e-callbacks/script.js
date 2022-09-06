e73caae7bbdb414168ce178e1f16b5a21e94413c
Leila Moraes <leila.violino@gmail.com>
2022-09-06 20:51:48 -0300

    resolução da questão 4

--- front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/script.js
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/script.js
@@ -81,3 +81,20 @@ const getPlanet = () => {
 };
 
 getPlanet();
+
+//Exercício 4
+
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+const sendMarsTemperature = () => {
+const delay = messageDelay();
+const temperature = getMarsTemperature();
+setTimeout(() => console.log(`A temperatura de Marte é: ${temperature} graus Celsius`), delay);
+};
+
+sendMarsTemperature();
