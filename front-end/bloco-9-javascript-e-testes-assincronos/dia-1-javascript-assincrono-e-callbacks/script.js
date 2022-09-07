14d272ba7d3cb961c8ddead19638009c3bbb36a2
Leila Moraes <leila.violino@gmail.com>
2022-09-06 23:17:27 -0300

    quase resolução da questão 6. Tá retornando coisas a mais e vou descobrir o porque depois

--- front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/script.js
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/script.js
@@ -131,3 +131,42 @@ const sendMarsTemperature = (callback) => {
 sendMarsTemperature(temperatureInFahrenheit);
 
 sendMarsTemperature(greet);
+
+//Exercício 6
+
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+}
+
+const toFahrenheit = (degreeCelsius) => (degreeCelsius * (9 / 5)) + 32;
+
+const temperatureInFahrenheit = (temperature) =>
+  console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
+
+const greet = (temperature) =>
+  console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
+
+const handleError = (errorReason) =>
+  console.log(`Error getting temperature: ${errorReason}`);
+
+// Função que eu criei
+
+const sendMarsTemperature = (success, notSuccess) => {
+  setTimeout(() => {
+    const didOperationSucceed = Math.random() >= 0.6;
+    if (didOperationSucceed){
+    const degrees = getMarsTemperature();
+    return success(degrees);
+    } else {
+      const errorMessage = handleError('Robot is busy');
+      return notSuccess(errorMessage);
+    }
+  }, messageDelay())
+}
+
+sendMarsTemperature(temperatureInFahrenheit, handleError);
+
+sendMarsTemperature(greet, handleError);
